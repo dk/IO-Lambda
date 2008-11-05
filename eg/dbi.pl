a3876aca5a85bc305a8e651803a2a2a2c02eb65f
dk <dk>
2008-11-05 12:40:34 +0000

    - cool additions

--- eg/dbi.pl
+++ eg/dbi.pl
@@ -1,10 +1,34 @@
-#$Id: dbi.pl,v 1.5 2008/11/04 19:04:08 dk Exp $
+#$Id: dbi.pl,v 1.6 2008/11/05 12:40:34 dk Exp $
 use strict;
 use warnings;
 
+use IO::Socket::INET;
 use IO::Lambda qw(:all);
 use IO::Lambda::DBI;
 use IO::Lambda::Thread qw(threaded);
+use IO::Lambda::Fork qw(forked);
+use IO::Lambda::Socket;
+
+my $port = 3333;
+
+sub usage
+{
+	print <<USAGE;
+
+Test implementation of non-blocking DBI. This script can work in several modes,
+run with one of the parameters to switch:
+
+   $0 thread      - use DBI calls in a separate thread
+   $0 fork        - use DBI calls in a separate process
+   $0 remote HOST - connect to host to port $port and request DBI there
+   $0 listen      - listen on port $port, execute incoming connections
+	
+USAGE
+	exit;
+}
+
+my $mode = shift(@ARGV) || '';
+usage unless $mode =~ /^(fork|thread|remote|listen)$/;
 
 sub check_dbi
 {
@@ -24,29 +48,78 @@ sub check_dbi
 	}}
 }
 
-my $t = threaded {
-	my $socket = shift;
-	IO::Lambda::Message::DBI-> new( $socket, $socket )-> run;
-};
+sub execute
+{
+	my $dbi = shift;
+	lambda {
+		context $dbi-> connect('DBI:mysql:database=mysql', '', '');
+		tail {
+			warn(@_), return unless shift;
+			context 
+				check_dbi($dbi),
+				check_dbi($dbi),
+				check_dbi($dbi);
+		tails {
+			context $dbi-> disconnect;
+		&tail();
+	}}}-> wait;
+}
 
-$t-> start;
-$t-> join_on_read(0);
+# run
 
-my $dbi = IO::Lambda::DBI-> new( $t-> socket, $t-> socket );
-lambda {
-	context $dbi-> connect('DBI:mysql:database=mysql', '', '');
-	tail {
-		warn(@_), return unless shift;
-		context 
-			check_dbi($dbi),
-			check_dbi($dbi),
-			check_dbi($dbi);
-	tails {
-		context $dbi-> disconnect;
-	&tail();
-}}}-> wait;
+if ( $mode eq 'thread') {
+	die $IO::Lambda::Thread::DISABLED if $IO::Lambda::Thread::DISABLED;
 
-undef $dbi;
+	my $t = threaded {
+		my $socket = shift;
+		IO::Lambda::Message::DBI-> new( $socket, $socket )-> run;
+	};
+	
+	$t-> start;
+	$t-> join_on_read(0);
+	
+	my $dbi = IO::Lambda::DBI-> new( $t-> socket, $t-> socket );
+	execute($dbi);
+	undef $dbi;
+	
+	$t-> join_on_read(1);
+	$t-> join;
+} elsif ( $mode eq 'fork') {
+	my $t = forked {
+		my $socket = shift;
+		IO::Lambda::Message::DBI-> new( $socket, $socket )-> run;
+	};
+	
+	$t-> start;
+	$t-> listen(0);
+	
+	my $dbi = IO::Lambda::DBI-> new( $t-> socket, $t-> socket );
+	execute($dbi);
+	undef $dbi;
+	
+	$t-> listen(1);
+	$t-> wait;
+} elsif ( $mode eq 'remote') {
+	my $host = shift @ARGV;
+	usage unless defined $host;
 
-$t-> join_on_read(1);
-$t-> join;
+	my $s = IO::Socket::INET-> new("$host:$port");
+	die $! unless $s;
+
+	my $dbi = IO::Lambda::DBI-> new( $s, $s);
+	execute($dbi);
+
+	undef $s;
+} elsif ( $mode eq 'listen') {
+	my $s = IO::Socket::INET-> new(
+		LocalPort => $port,
+		Listen    => 5,
+	);
+	while ( 1) {
+		my $c = IO::Handle-> new;
+		die $! unless accept( $c, $s);
+		my $loop = IO::Lambda::Message::DBI-> new( $c, $c);
+		$loop-> run;
+		close($c);
+	}
+}
